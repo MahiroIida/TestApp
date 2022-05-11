a770cf4f9ebd0dbb753c542f281aea462ab8e177
Mahiro iida <point.mahi2523@icloud.com>
2022-05-12 06:19:03 +0900

    First Commit

---
+++ TestAPP/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TestAPP
+//
+//  Created by 飯田真裕 on 2022/05/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Test")
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
